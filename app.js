aeab1437e99c4e40f721d5cbc3bd28393f6a15c6
mooksdujour <jmooking@hotmail.com>
2019-11-07 16:23:49 -0500

    add mongo atlas

--- app.js
+++ app.js
@@ -17,7 +17,17 @@ const commentRoutes    = require("./routes/comments"),
       campgroundRoutes = require("./routes/campgrounds"),
       indexRoutes      = require("./routes/index")
  
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://TheMooGuy:%21amLook1ng@cluster0-miakj.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).then(() =>{
+    console.log('Connected to DB!');
+}).catch(err => {
+    console.log('ERROR: ', err.mesage);
+});
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
